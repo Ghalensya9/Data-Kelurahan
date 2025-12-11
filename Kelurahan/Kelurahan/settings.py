880967f91383c67f1506cdfb55c5819dcb4fd09d
Ghalensya9 <ghalensyan@gmail.com>
2025-12-11 10:54:03 +0700

    Praktikum 10

--- Kelurahan/Kelurahan/settings.py
+++ Kelurahan/Kelurahan/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'warga',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -128,9 +129,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2, 
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
 
 }
--- Kelurahan/warga/views.py
+++ Kelurahan/warga/views.py
@@ -1,29 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView,UpdateView
-from .models import Warga ,Pengaduan
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
 from django.urls import reverse_lazy
+
+from .models import Warga, Pengaduan
 from .forms import PengaduanForm, WargaForm
+
+from rest_framework import viewsets, generics
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework import viewsets # Impor viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from .serializers import WargaSerializer, PengaduanSerializer
-from .models import Warga
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly # Impor ini
-from rest_framework.permissions import IsAdminUser 
+from django_filters.rest_framework import DjangoFilterBackend
 
-class WargaListAPI(ListAPIView):
-    queryset = Warga.objects.all()
-    serializer_class = WargaSerializer
 
+# ==== View berbasis template biasa ====
 
 class WargaListView(ListView):
     model = Warga
 
+
 class WargaDetailView(DetailView):
     model = Warga
 
+
 class PengaduanListView(ListView):
     model = Pengaduan
+
 
 class WargaCreateView(CreateView):
     model = Warga
@@ -31,27 +36,32 @@ class WargaCreateView(CreateView):
     template_name = 'warga/warga_form.html'
     success_url = reverse_lazy('warga_list')
 
+
 class PengaduanCreateView(CreateView):
     model = Pengaduan
     form_class = PengaduanForm
     template_name = 'warga/pengaduan_form.html'
     success_url = reverse_lazy('pengaduan_list')
 
+
 class WargaUpdateView(UpdateView):
     model = Warga
     form_class = WargaForm
-    template_name = 'warga/warga_form.html' # Kita pakai template yang sama
+    template_name = 'warga/warga_form.html'
     success_url = reverse_lazy('warga_list')
+
 
 class WargaDeleteView(DeleteView):
     model = Warga
     template_name = 'warga/warga_confirm_delete.html'
     success_url = reverse_lazy('warga_list')
 
+
 class PengaduanDeleteView(DeleteView):
     model = Pengaduan
     template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan_list')
+
 
 class PengaduanUpdateView(UpdateView):
     model = Pengaduan
@@ -59,25 +69,36 @@ class PengaduanUpdateView(UpdateView):
     template_name = 'warga/pengaduan_form.html'
     success_url = reverse_lazy('pengaduan_list')
 
+
+# ==== API berbasis DRF (generic views) ====
+
 class WargaListAPIView(generics.ListAPIView):
     queryset = Warga.objects.all()
     serializer_class = WargaSerializer
 
-#class WargaDetailAPIView(RetrieveAPIView):
-    #queryset = Warga.objects.all()
-    #serializer_class = WargaSerializer
+
+# class WargaDetailAPIView(RetrieveAPIView):
+#     queryset = Warga.objects.all()
+#     serializer_class = WargaSerializer
+
+
+# ==== API berbasis ViewSet (untuk router /api/...) ====
 
 class WargaViewSet(viewsets.ModelViewSet):
     queryset = Warga.objects.all().order_by('-tanggal_registrasi')
     serializer_class = WargaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    # Removed DjangoFilterBackend to avoid missing template error
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['nama_lengkap', 'nik', 'alamat']
+    ordering_fields = ['nama_lengkap', 'tanggal_registrasi']
 
-# Tambahkan ViewSet untuk model Pengaduan
 class PengaduanViewSet(viewsets.ModelViewSet):
-    queryset = Pengaduan.objects.all()  # <--- Baris ini hilang di kode Anda
+    queryset = Pengaduan.objects.all()
     serializer_class = PengaduanSerializer
     permission_classes = [IsAdminUser]
 
-class WargaViewSet(viewsets.ModelViewSet):
-    queryset = Warga.objects.all().order_by('-tanggal_registrasi')
-    serializer_class = WargaSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly] # Timpa izin default
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['judul', 'isi_pengaduan', 'status']     
+    ordering_fields = ['tanggal_pengaduan', 'status']
+    # Filter bawaan DRF (tanpa DjangoFilterBackend
